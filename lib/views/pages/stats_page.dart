f82723b1b02cf7d5c25dc0bee09e809912cb1d34
Fahim Foysal <iamzero097@gmail.com>
2025-08-22 00:25:55 +0600

    bar chart added

--- lib/views/pages/stats_page.dart
+++ lib/views/pages/stats_page.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:spendle/views/stats/chart.dart';
+import 'package:spendle/views/stats/pie_chart.dart';
 
 class StatsPage extends StatelessWidget {
   const StatsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Stack(children: [Center(child: Text("Stats Coming Soon"))]);
+    return SingleChildScrollView(
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsetsGeometry.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Charts",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 5),
+              Container(
+                padding: const EdgeInsets.all(4),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width,
+                child: const ABarChart(),
+              ),
+              const SizedBox(height: 50),
+              Container(
+                padding: const EdgeInsets.all(4),
+                // child: const ExpensePieChart(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/views/stats/chart.dart
@@ -0,0 +1,160 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:spendle/database/database_helper.dart';
+import 'package:intl/intl.dart';
+
+class ABarChart extends StatefulWidget {
+  const ABarChart({super.key});
+
+  @override
+  State<ABarChart> createState() => _ABarChartState();
+}
+
+class _ABarChartState extends State<ABarChart> {
+  // Holds last 7 days of expenses totals
+  List<double> dailyTotals = List.filled(7, 0);
+
+  @override
+  void initState() {
+    super.initState();
+    loadExpenses();
+  }
+
+  Future<void> loadExpenses() async {
+    final db = await DatabaseHelper().database;
+    final data = await db.query('expenses', orderBy: 'date ASC');
+
+    // Prepare last 7 days keys
+    final now = DateTime.now();
+    Map<String, double> totals = {};
+    for (int i = 0; i < 7; i++) {
+      final day = DateFormat(
+        'yyyy-MM-dd',
+      ).format(now.subtract(Duration(days: 6 - i)));
+      totals[day] = 0;
+    }
+
+    for (var row in data) {
+      final dateStr = row['date'] as String;
+      final amount = (row['amount'] as num).toDouble();
+      if (totals.containsKey(dateStr)) {
+        totals[dateStr] = totals[dateStr]! + amount;
+      }
+    }
+
+    setState(() {
+      dailyTotals = totals.values.toList();
+    });
+  }
+
+  LinearGradient get _barsGradient => const LinearGradient(
+    colors: [Colors.blue, Colors.cyan],
+    begin: Alignment.bottomCenter,
+    end: Alignment.topCenter,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return BarChart(
+      BarChartData(
+        barTouchData: BarTouchData(
+          enabled: false,
+          touchTooltipData: BarTouchTooltipData(
+            getTooltipColor: (group) => Colors.transparent,
+            tooltipPadding: EdgeInsets.zero,
+            tooltipMargin: 8,
+            getTooltipItem:
+                (
+                  BarChartGroupData group,
+                  int groupIndex,
+                  BarChartRodData rod,
+                  int rodIndex,
+                ) {
+                  return BarTooltipItem(
+                    rod.toY.round().toString(),
+                    const TextStyle(
+                      color: Colors.cyan,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  );
+                },
+          ),
+        ),
+
+        titlesData: FlTitlesData(
+          show: true,
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              reservedSize: 30,
+              getTitlesWidget: getTitles,
+            ),
+          ),
+          leftTitles: const AxisTitles(
+            sideTitles: SideTitles(showTitles: false),
+          ),
+          topTitles: const AxisTitles(
+            sideTitles: SideTitles(showTitles: false),
+          ),
+          rightTitles: const AxisTitles(
+            sideTitles: SideTitles(showTitles: false),
+          ),
+        ),
+        borderData: FlBorderData(show: false),
+        gridData: const FlGridData(show: false),
+        alignment: BarChartAlignment.spaceAround,
+        maxY: dailyTotals.isEmpty
+            ? 20
+            : (dailyTotals.reduce((a, b) => a > b ? a : b) * 1.2),
+        barGroups: List.generate(7, (i) {
+          return BarChartGroupData(
+            x: i,
+            barRods: [
+              BarChartRodData(toY: dailyTotals[i], gradient: _barsGradient),
+            ],
+            showingTooltipIndicators: [0],
+          );
+        }),
+      ),
+    );
+  }
+
+  Widget getTitles(double value, TitleMeta meta) {
+    final style = TextStyle(
+      color: Colors.blue[800],
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    );
+    String text;
+    switch (value.toInt()) {
+      case 0:
+        text = 'Mn';
+        break;
+      case 1:
+        text = 'Te';
+        break;
+      case 2:
+        text = 'Wd';
+        break;
+      case 3:
+        text = 'Tu';
+        break;
+      case 4:
+        text = 'Fr';
+        break;
+      case 5:
+        text = 'St';
+        break;
+      case 6:
+        text = 'Sn';
+        break;
+      default:
+        text = '';
+    }
+    return SideTitleWidget(
+      meta: meta,
+      space: 4,
+      child: Text(text, style: style),
+    );
+  }
+}
